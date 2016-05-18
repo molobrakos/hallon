2f4d7edbedc409138515a92ba883dd5d0675bb06
Erik <erik.eriksson@yahoo.com>
2016-05-18 10:20:19 +0200

    use robobrowser

--- hallon.py
+++ hallon.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 extract usage data from hallon mobile broadband
 """
 
-from mechanize import Browser
+from robobrowser import RoboBrowser
 from bs4 import BeautifulSoup as Soup
 from os import path
 import sys
@@ -16,21 +16,20 @@ with open(path.join(path.dirname(sys.argv[0]),
     CREDENTIALS = yaml.safe_load(f)
 
 URL = "https://www.hallon.se/mina-sidor"
-br = Browser()
+br = RoboBrowser(parser="lxml")
 br.open(URL)
-br.form = [fr for fr in br.forms() if "logga-in" in fr.action][0]
-br.form["UserName"] = CREDENTIALS["username"]
-br.form["Password"] = CREDENTIALS["password"]
-br.submit()
+form = br.get_form(action="/logga-in")
+form["UserName"].value = CREDENTIALS["username"]
+form["Password"].value = CREDENTIALS["password"]
+br.submit_form(form)
 
-page = Soup(br.response().read(), "html.parser")
-usage = page.select("p.usage")[0].text.replace(",", ".").split()
+usage = br.select("p.usage")[0].text.replace(",", ".").split()
 
 remaining = round(float(usage[0]), 2)
 total = int(usage[2])
 used = round(float(total-remaining), 2)
 used_pct = round(used*100/total, 1)
-days_remaining = int(page.select("p.usage-daysleft")[0].text.split()[0])
+days_remaining = int(br.select("p.usage-daysleft")[0].text.split()[0])
 
 print(json.dumps({"total": total,
                   "remaining": remaining,
